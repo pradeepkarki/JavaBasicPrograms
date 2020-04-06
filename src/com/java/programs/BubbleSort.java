f1a8b39291923ac14d2ca7794a0d32869ae6a86e
pradeepkarki <pradeepkarki70@gmail.com>
2020-04-07 00:26:01 +0530

    Comments

--- src/com/java/programs/BubbleSort.java
+++ src/com/java/programs/BubbleSort.java
@@ -1,40 +1,36 @@
 package com.java.programs;
 
-public class BubbleSort
-{
-
-	public static void main(String[] args)
-	{
-		int arr[] =
-		{ 2, 9, 13, 10, 15, 34, 5, 7 };
+public class BubbleSort {
+	/**
+	 * Bubble sort program
+	 * 
+	 * 
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int arr[] = { 2, 9, 13, 10, 15, 34, 5, 7 };
 
 		bubbleSort(arr);
 	}
 
-	private static void bubbleSort(int[] arr)
-	{
+	private static void bubbleSort(int[] arr) {
 		int size = arr.length;
 		int temp;
-		for (int i = 0; i < size; i++)
-		{
-			for (int j = 0; j < size - i - 1; j++)
-			{
-				if (arr[j] > arr[j + 1])
-				{
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
 					temp = arr[j];
 					arr[j] = arr[j + 1];
 					arr[j + 1] = temp;
 				}
 			}
 		}
-		
-		
+
 		System.out.print("Array elements - ");
-		for (int i = 0; i < arr.length; i++)
-		{
-			System.out.print(arr[i]+",");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + ",");
 		}
-		
 
 	}
 }
